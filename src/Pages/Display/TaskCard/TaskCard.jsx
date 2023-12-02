cee4c3a38e08da3d7fb84639441ab7a5f3778b33
nazmul906 <nazmulhb96@gmail.com>
2023-12-02 20:37:07 +0600

    delete task functionality is added and immediately rendred updated tasklist in ui

--- src/Pages/Display/TaskCard/TaskCard.jsx
+++ src/Pages/Display/TaskCard/TaskCard.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import "./taskcard.css";
 import Swal from "sweetalert2";
+import AllTask from "../../Home/AllTask/AllTask";
 
-const TaskCard = ({ alltask }) => {
+const TaskCard = ({ alltask, onDelete }) => {
   const { _id, title, description, status } = alltask;
 
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -45,6 +46,19 @@ const TaskCard = ({ alltask }) => {
     setEditItemId(null);
   };
 
+  const handledelete = (_id) => {
+    fetch(`http://localhost:5000/deletetask/${_id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.deletedCount > 0) {
+          Swal.fire("Deleted Successfully");
+          onDelete(_id);
+        }
+      });
+  };
   return (
     <div className="card w-96 bg-white h-full shadow-2xl rounded-lg p-4">
       <div className="card-body items-center text-center">
@@ -58,7 +72,10 @@ const TaskCard = ({ alltask }) => {
           >
             edit
           </button>
-          <button className="w-32 mt-4 bg-indigo-500 text-white font-semibold px-4 py-2 rounded-md hover:bg-indigo-700 transition duration-300 ease-in-out">
+          <button
+            className="w-32 mt-4 bg-indigo-500 text-white font-semibold px-4 py-2 rounded-md hover:bg-indigo-700 transition duration-300 ease-in-out"
+            onClick={() => handledelete(_id)}
+          >
             delete
           </button>
         </div>
--- src/Pages/Home/AllTask/AllTask.jsx
+++ src/Pages/Home/AllTask/AllTask.jsx
@@ -11,12 +11,20 @@ const AllTask = () => {
       .then((res) => res.json())
       .then((data) => setAllTask(data));
   }, []);
+  const handleDeleteTask = (taskId) => {
+    const remainingTasks = alltask.filter((task) => task._id !== taskId);
+    setAllTask(remainingTasks);
+  };
 
   return (
     <div className="flex justify-center">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 text-center gap-[2.5rem] mb-2 p-[2.5rem]">
         {alltask.map((item) => (
-          <TaskCard key={item._id} alltask={item}></TaskCard>
+          <TaskCard
+            key={item._id}
+            alltask={item}
+            onDelete={handleDeleteTask}
+          ></TaskCard>
         ))}
       </div>
     </div>
